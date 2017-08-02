793bd11c1219924c4efca3075e914da932fb5f0a
David Etzel <etzeldc@gmail.com>
2017-08-02 16:46:00 -0600

    Calculator Cont...
    Added functionality to most keys and formatted calculator display

--- index.js
+++ index.js
@@ -92,3 +92,139 @@ function expClear() {
     $('#expOutput').empty();
     $('#expClear').hide();
 }
+
+// CALCULATOR KEYS
+var screen = "";
+var memory = 0;
+var operation = "";
+function ac() {
+    screen = "";
+    memory = 0;
+    operation = "";
+    $('#calcOutput').empty();
+}
+
+// Number first, or just splice the string?
+// function invert() {
+//     screen = screen + "9";
+//     $('#calcOutput').empty();
+//     $('#calcOutput').append(screen);
+// }
+
+// Splice a period two before the end (toggle on/off)
+// function percent() {
+//     screen = screen + "9";
+//     $('#calcOutput').empty();
+//     $('#calcOutput').append(screen);
+// }
+
+function divide() {
+    memory = screen;
+    screen = "";
+    operation = "/";
+    $('#calcOutput').empty();
+}
+
+function seven() {
+    screen = screen + "7";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+function eight() {
+    screen = screen + "8";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+function nine() {
+    screen = screen + "9";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+function multiply() {
+    memory = screen;
+    screen = "";
+    operation = "*";
+    $('#calcOutput').empty();
+}
+
+function four() {
+    screen = screen + "4";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+function five() {
+    screen = screen + "5";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+function six() {
+    screen = screen + "6";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+function subtract() {
+    memory = screen;
+    screen = "";
+    operation = "-"
+    $('#calcOutput').empty();
+}
+
+function one() {
+    screen = screen + "1";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+function two() {
+    screen = screen + "2";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+function three() {
+    screen = screen + "3";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+function add() {
+    memory = screen;
+    screen = "";
+    operation = "+";
+    $('#calcOutput').empty();
+}
+
+function zero() {
+    screen = screen + "0";
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+// Slice the end of the string
+function backspace() {
+    screen = screen
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+function decimal() {
+    if (screen === "") {
+        screen = screen + "0.";
+    } else {
+    screen = screen + ".";
+    }
+    $('#calcOutput').empty();
+    $('#calcOutput').append(screen);
+}
+
+// Operations are covered in the if conditions
+// function equals() {
+//     result = memory + operation + screen;
+// }
+
+// // tests
+// console.log(screen);
+// console.log(memory);
+// console.log(operation);
