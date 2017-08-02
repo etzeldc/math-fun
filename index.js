e234b532e2b38dce63796aa0316821d3f09a6ff7
David Etzel <etzeldc@gmail.com>
2017-08-02 13:14:47 -0600

    More Functions
    Added sum/exponent functionality and clear values buttons

--- index.js
+++ index.js
@@ -1,35 +1,94 @@
-// AVERAGE
+// SUM
 var sum = 0;
+function getSum() {
+    addThis = Number($('#addThis').val());
+    $('#addThis').val("");
+    sum = sum + addThis;
+    $('#sumOutput').empty();
+    $('#sumOutput').append(sum);
+    $('#sumClear').show();
+}
+
+function sumClear() {
+    $('#sumOutput').empty();
+    $('#sumClear').hide();
+}
+
+// AVERAGE
+var avgSum = 0;
 var number = 0;
 function getAvg() {
-    var addThis = Number($('#addThis').val());
-    $('#addThis').val("");
+    avgThis = Number($('#avgThis').val());
+    $('#avgThis').val("");
     $('#avgOutput').empty();
-    sum = sum + addThis;
+    avgSum = avgSum + avgThis;
     number = number + 1;
-    newAvg = sum / number;
+    newAvg = avgSum / number;
     $('#avgOutput').append(newAvg);
+    $('#avgClear').show();
 }
 
-// SQUARE
+function avgClear() {
+    $('#avgOutput').empty();
+    $('#avgClear').hide();
+}
+
+// SQUARE AND CUBE
 function square() {
-    var square = Number($('#sqcube').val()) * Number($('#sqcube').val());
+    square = Number($('#sqcube').val()) * Number($('#sqcube').val());
+    $('#sqcube').val("");
     $('#sqOutput').empty();
     $('#sqOutput').append(square);
+    $('#sqCubeClear').show();
 }
 
- // CUBE
 function cube() {
-    var cube = Number($('#sqcube').val()) * Number($('#sqcube').val()) * Number($('#sqcube').val());
+    cube = Number($('#sqcube').val()) * Number($('#sqcube').val()) * Number($('#sqcube').val());
+    $('#sqcube').val("");
     $('#cubeOutput').empty();
     $('#cubeOutput').append(cube);
+    $('#sqCubeClear').show();
+}
+
+function sqCubeClear() {
+    $('#sqOutput').empty();
+    $('#cubeOutput').empty();
+    $('#sqCubeClear').hide();
 }
 
 // PYTHAGOREAN 
 function hypotenuse() {
-    var sideA = Number($('#sideA').val());
-    var sideB = Number($('#sideB').val());
-    var hypot = Math.sqrt((sideA * sideA) + (sideB * sideB));
+    sideA = Number($('#sideA').val());
+    sideB = Number($('#sideB').val());
+    hypot = Math.sqrt((sideA * sideA) + (sideB * sideB));
+    $('#sideA').val("");
+    $('#sideB').val("");
     $('#hypotOutput').empty();
     $('#hypotOutput').append(hypot);
+    $('#hypotClear').show();
+}
+
+function hypotClear() {
+    $('#hypotOutput').empty();
+    $('#hypotClear').hide();
+}
+
+// EXPONENTS
+function exponent() {
+    base = Number($('#base').val());
+    power = Number($('#power').val());
+    product = 1;
+    for (var i = 0; i < power; i++) {
+        product = product * base;
+    };
+    $('#base').val("");
+    $('#power').val("");
+    $('#expOutput').empty();
+    $('#expOutput').append(product);
+    $('#expClear').show();
+}
+
+function expClear() {
+    $('#expOutput').empty();
+    $('#expClear').hide();
 }
